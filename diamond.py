5180ef49f8d6611d8ab4dced521d377ed722c368
JossNockolds <19121867@hlnsc.ac.uk>
2023-11-05 11:55:23 +0000

    checks if n is valid

--- diamond.py
+++ diamond.py
@@ -1,21 +1,37 @@
 def diamond(n):
-    import math
-    diamonds_on_row=1
-    diamond_row=''
-    diamond=''
-    for row in range(1,n+1):
-        gap=(int((n-diamonds_on_row)/2))
-        for i in range(gap):
-            diamond+=' '
-        for i in range(diamonds_on_row):
-            diamond+='*'
-        for i in range(gap):
-            diamond+=' '
-        diamond+='\n'
-        if row<math.ceil(n/2):
-            diamonds_on_row+=2
+    if type(n) is int:
+        if n<0:
+            return(None)
+        elif n%2==0:
+            return(None)
         else:
-            diamonds_on_row-=2
-    return(diamond)
+            import math
+            diamonds_on_row=1
+            diamond_row=''
+            diamond=''
+            for row in range(1,n+1):
+                gap=(int((n-diamonds_on_row)/2))
+                for i in range(gap):
+                    diamond+=' '
+                for i in range(diamonds_on_row):
+                    diamond+='*'
+                diamond+='\n'
+                if row<math.ceil(n/2):
+                    diamonds_on_row+=2
+                else:
+                    diamonds_on_row-=2
+            return(diamond)
+    else:
+        return(None)
 
-print(diamond(10))
+
+
+
+
+
+    
+
+
+
+
+print(diamond(13))
